0c8adead20aaf3f7184f8724e8340ba6b34c1cd5
MOTHINK <mohamedadelbenmimoun@gmail.com>
2024-07-28 00:49:59 +0200

    {date}

--- app/src/main/java/com/example/luckylotto/ui/view/ProfileScreen.kt
+++ app/src/main/java/com/example/luckylotto/ui/view/ProfileScreen.kt
@@ -1,22 +1,21 @@
 package com.example.luckylotto.ui.view
 
-import android.widget.Space
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -25,9 +24,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -64,31 +65,38 @@ private fun ProfileCard(modifier: Modifier) {
                 .height(300.dp)
                 .padding(10.dp)
         ) {
-            Column(modifier = modifier.height(300.dp)) {
-                Column {
-                    Row(
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(Color.DarkGray)
+            ) {
+                Row(
+                    modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceAround
+                ) {
+                    Box(modifier = modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(Color.Gray)) {
+                        PurchaseCoins()
+                    }
+                    Box(
                         modifier = modifier
-                            .background(Color.Black)
-                            .fillMaxWidth()
+                            .height(120.dp)
+                            .width(120.dp),
+                        contentAlignment = Alignment.Center
                     ) {
-                        Box(
-                            modifier = modifier
-                                .background(Color.Red)
-                                .height(200.dp)
-                                .width(100.dp)
-                        )
-                        Box(
-                            modifier = modifier
-                                .background(Color.Blue)
-                                .height(200.dp)
-                                .width(100.dp)
-                        )
-                        Box(
-                            modifier = modifier
-                                .background(Color.Yellow)
-                                .height(200.dp)
-                                .width(100.dp)
-                        )
+                        UserLogo(modifier)
+                    }
+                    Box(modifier = modifier
+                        .height(100.dp)
+                        .width(100.dp)
+                        .background(Color.Gray),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        MailCard()
                     }
                 }
             }
@@ -96,44 +104,100 @@ private fun ProfileCard(modifier: Modifier) {
     }
 }
 
-/*
+@Composable
+private fun UserLogo(modifier: Modifier) {
+    AsyncImage(
+        model = FirebaseAuthentication.instance.getFirebaseCurrentUser()?.photoUrl.toString(),
+        modifier = modifier
+            .size(100.dp)
+            .clip(CircleShape),
+        contentDescription = "User image profile"
+    )
+}
 
-Column {
-                Box(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .height(120.dp)
-                ) {
-                    AsyncImage(
-                        model = FirebaseAuthentication.instance.getFirebaseCurrentUser()?.photoUrl.toString(),
-                        modifier = modifier
-                            .size(100.dp)
-                            .clip(CircleShape)
-                            .background(Color.Red)
-                            .align(Alignment.Center),
-                        contentDescription = "User image profile"
-                    )
-                }
-                Box(
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .height(50.dp),
-                ) {
-                    Text(
-                        modifier = modifier.align(Alignment.Center),
-                        text = "Welcome: " + FirebaseAuthentication.instance.getFirebaseCurrentUser()?.displayName.toString(),
-                        color = Color.White,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
 
-                        )
-                }
-                Box(
-                    modifier = modifier
-                        .background(Color.Blue)
-                        .fillMaxWidth()
-                        .height(130.dp),
-                )
-            }
+@Composable
+private fun MailCard() {
+    IconButton(
+        modifier =  Modifier
+            .size(85.dp),
+        onClick = { /* Handle button click */ }
+    ) {
+        MessageCounter()
+    }
+}
 
- */
+@Composable
+private fun PurchaseCoins() {
+    IconButton(
+        modifier =  Modifier
+            .size(85.dp),
+        onClick = { /* Handle button click */ }
+    ) {
+        // Code here next...
+    }
+}
+
+@Composable
+private fun JustAnImage(modifier: Modifier) {
+    Image(
+        modifier =  modifier
+            .size(50.dp),
+        painter = painterResource(id = R.drawable.mail),
+        contentScale = ContentScale.Crop,
+        contentDescription = "Mail"
+    )
+}
+
+@Composable
+private fun MessageCounter() {
+    val size = 70
+    Box(
+        modifier = Modifier
+            .size(size.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Image(
+            modifier =  Modifier
+                .size(size.dp),
+            painter = painterResource(id = R.drawable.mail),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Mail"
+        )
+        Box(
+            modifier = Modifier
+                .offset(x = 20.dp, y = (-10).dp)
+                .clip(CircleShape)
+                .background(Color.Red)
+        ) {
+            Text(
+                modifier = Modifier.size(30.dp)
+                    .align(Alignment.Center)
+                    .padding(0.dp,3.dp),
+                textAlign = TextAlign.Center,
+                text = "+9",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CoinCounter() {
+    val size = 70
+    Box(
+        modifier = Modifier
+            .size(size.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Image(
+            modifier =  Modifier
+                .size(size.dp),
+            painter = painterResource(id = R.drawable.coin),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Mail"
+        )
+    }
+}
